c0f233bc4a211489836f01f67219c5492736c398
ondrejicka4 <m.ondrejicka@gmail.com>
2018-07-22 20:34:05 +0200

    Initial Commit

---
+++ app/src/main/java/com/example/michal/coastergram/Models/User.java
@@ -0,0 +1,8 @@
+package com.example.michal.coastergram.Models;
+
+/**
+ * Created by Michal on 22/07/2018.
+ */
+
+public class User {
+}
---
+++ app/src/main/java/com/example/michal/coastergram/Utilities/WSCommunicator.java
@@ -0,0 +1,8 @@
+package com.example.michal.coastergram.Utilities;
+
+/**
+ * Created by Michal on 22/07/2018.
+ */
+
+public class WSCommunicator {
+}
